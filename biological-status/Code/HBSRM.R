5d0e094e6300ba0473f9c00c9421272edf8e4c06
Bruno Carturan <bcarturan@psf.ca>
2023-08-28 20:24:23 -0700

    Almost done producing the figure.

--- biological-status/Code/HBSRM.R
+++ biological-status/Code/HBSRM.R
@@ -14,8 +14,10 @@ source("Code/linear_SR_func.R")
 library(R2jags)  # Provides wrapper functions to implement Bayesian analysis in JAGS.
 library(modeest) # Provides estimators of the mode of univariate data or univariate distributions.
 
-# Define subdirectories:
+# option to export the figures
+print_fig <- F
 
+# Define subdirectories:
 wd_Code <- paste0(getwd(),"/Code")
 wd_Data <- paste0(getwd(),"/Data")
 wd_Figures <- paste0(getwd(),"/Figures")
@@ -114,11 +116,11 @@ fndata <- paste(wd_Data_input,fndata,sep = "/")
 FBYr <- -99   # set first brood year, "-99" for no constraint
 MinSRpts <- 3 # set minimum # of SR data points required to be included in the analysis
 
-#---------------------------------------------------------------#
-# Read in Stock-Recruit Data, run the HBSR model and output paraemter estimates ----
-#---------------------------------------------------------------#
-
 for(i in 1:length(Species)){
+  
+  #----------------------------------------------------------------------------#
+  # Read in Stock-Recruit Data, run the HBSR model and output parameter estimates ----
+  #----------------------------------------------------------------------------#
   
   print(paste0("*** Plot printer for: ",
                colnames(species_acronym[, species_acronym == Species[i],drop=F]),
@@ -334,23 +336,79 @@ for(i in 1:length(Species)){
   
   # i <- 1
   # Import the ...
+  # could also simply use mypost
   post <- read.table(file = paste0(wd_Output,"/",region,".",Species[i],".post.out"),
                      header = T)
   
   # maximum nb of CUs
   # MaxStocks <- scan(file = fndata[i],nlines = 1,skip = 1)
   
-  
-  # for each CU...
-  for(cu in 1:length(unique(d$CU))){
+  # for each CU..
+  CUs <- unique(d$CU)
+  for(i_cu in 1:length(CUs)){
     
-    # cu <- 1
-    d_sub <- subset(x = d, subset = CU == unique(d$CU)[cu])
+    # i_cu <- 1
+    CU <-CUs[i_cu]
+    d_sub <- subset(x = d, subset = CU == CU)
     
+    # alpha <- sx.post[,16]; beta <- sx.post[,65] # ?! what are those big numbers in there?
+    alpha <- post[,i_cu]
+    beta <- post[,i_cu + length(unique(d$CU))]
     
+    # 
+    spw <- seq(0,max(d_sub$Esc, na.rm = T),100)
     
+    # 
+    recruits <- array(NA, dim = c(1,length(spw), 10000))
     
+    # 
+    for(j in 1:10000){
+      iter <- sample(length(alpha),1)
+      recruits[,,j] <- spw * exp(alpha[iter] - beta[iter] * spw)
+    }
     
+    # 
+    rec.m <- apply(recruits,c(1,2),median)/1000
+    rec.u <- apply(recruits,c(1,2),quantile,probs = 0.975)/1000
+    rec.l <- apply(recruits,c(1,2),quantile,probs = 0.025)/1000
+    
+    if(print_fig){
+      # jpeg("LongSR.jpg", width=8, height=4,units="in",res=200)
+      # BSC: not sure if we want to export the figures in the github repo.
+      jpeg(paste0(wd_Figures,"/",region,"_",Species[i],"_",unique(d$CU)[i_cu],".jpg"), 
+           width = 8, height = 4,units = "in",res = 200)
+    }
+    
+    par(mfrow = c(1,1),mar = c(1.5,2.5,1,1.5),oma = c(2,2,2,1), new = F)
+    plot(x = d_sub$Esc / 1000,y = d_sub$Rec/1000,
+         bty='l', xlab="", ylab="", xaxt="n", yaxt="n", col = "white")
+    axis(1)
+    axis(2,las = 2)
+    # mtext("Sockeye - Long",3,cex=0.85,line=1)
+    species_full <- colnames(species_acronym)[which(species_acronym == Species[i])]
+    mtext(paste0(species_full," - ",CU),3,cex=0.85,line=1)
+    #
+    polygon(x = c(spw/1000,rev(spw/1000)),y = c(rec.u,rev(rec.l)),
+            col = grey(0.9),border = NA)
+    # 
+    points(x = d_sub$Esc / 1000,y = d_sub$Rec/1000)
+    #
+    lines(x = spw/1000, y = rec.m, lwd = 2)
+    
+    # adjustcolor(grey(0.9), alpha.f = 0.9)
+    abline(v =41.4,col="red",lwd=2)
+    abline(v =29.8,col="red",lty=2)
+    abline(v =76.7,col="red",lty=2)
+    
+    abline(v=82.8,col="dark green",lwd=2)
+    abline(v=59.7,col="dark green",lty=2)
+    abline(v=153.4,col="dark green",lty=2)
+    mtext("Spawners (000s)",1,outer=T,line=1)
+    mtext("Recruits (000s)",2,outer=T,line=1)
+    
+    if(print_fig){
+      dev.off()
+    }
     
   }
   

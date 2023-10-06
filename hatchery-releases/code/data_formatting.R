e941a92fa690647b329b138192fc224d749729d2
Bruno Carturan <bcarturan@psf.ca>
2023-10-06 13:26:51 -0700

    Check the correspondance of fields between the DFO dataset and the template. I am now waiting to hear from Eric to proceed further

--- hatchery-releases/code/data_formatting.R
+++ hatchery-releases/code/data_formatting.R
@@ -4,9 +4,103 @@ rm(list = ls())
 graphics.off()
 
 # Set directory to /biological-status
-if(!grepl(pattern = "biological-status", x = getwd())){
-  setwd(dir = paste0(getwd(),"/biological-status"))
+if(!grepl(pattern = "hatchery-releases", x = getwd())){
+  setwd(dir = paste0(getwd(),"/hatchery-releases"))
 }
+
+
+# Define subdirectories:
+wd_code <- paste0(getwd(),"/code")
+wd_data <- paste0(getwd(),"/data")
+
+
+library(readxl)
+library(tidyverse)
+
+# NOTES:
+# Eric: the only trick will be translating the "STOCK_CU_INDEX" field into 
+# "cuid_broodstock". Will have to use one of the tables in the decoder repo. 
+# Let me know if you have any other questions.
+
+# decoder repo:
+# C:\Users\bcarturan\Salmon Watersheds Dropbox\Bruno Carturan\X Drive\1_PROJECTS\1_Active\Population Methods and Analysis\decoders
+
+# Data obtained from DFO:
+dataDFO <- read_excel(paste0(wd_data,"/PSF_modified_SEP_releases_2023.xlsx"),
+                      sheet = 1)
+
+head(dataDFO)
+colnames(dataDFO)
+# apply(X = dataDFO, MARGIN = 2, FUN = unique)
+dataDFO$TotalRelease
+
+
+# Template:
+templSheet1 <- read_excel(paste0(wd_data,"/SWP_hatchery_data_template.xlsx"),
+                          sheet = "DataProvider")
+templSheet2 <- read_excel(paste0(wd_data,"/SWP_hatchery_data_template.xlsx"),
+                          sheet = "DataEntry_facilities")
+templSheet3 <- read_excel(paste0(wd_data,"/SWP_hatchery_data_template.xlsx"),
+                          sheet = "DataEntry_facilitiescuids")
+templSheet4 <- read_excel(paste0(wd_data,"/SWP_hatchery_data_template.xlsx"),
+                          sheet = "DataEntry_releases")
+
+# Sheet 1 -----
+templSheet1
+dim(templSheet1)
+fields_s1 <- templSheet1$`Pacific Salmon Foundation Salmon Watersheds Program` 
+fields_s1 <- fields_s1[!is.na(fields_s1)]
+fields_s1 <- fields_s1[3:length(fields_s1)]
+
+# "First Name"         --> ???
+# "Last Name"          --> ???
+# "Organization"       --> ??? 
+# "Email"              --> ???
+# "Metadata file name" --> ???
+unique(dataDFO$PROJ_NAME)
+unique(dataDFO$REL_CU_NAME)
+unique(dataDFO$RELEASE_SITE_NAME)
+
+# Sheet 2 ----
+colnames(templSheet2)
+
+# "facilityid" ???
+
+# "program"
+unique(dataDFO$PROGRAM_CODE)
+
+# "project"
+unique(dataDFO$PROJ_NAME)
+
+# "facilityname"
+unique(dataDFO$FACILITY_NAME)  # ???
+
+# "CUID"
+unique(dataDFO$STOCK_CU_ID)
+
+# "facility_latitude"
+unique(dataDFO$FACILITY_LATITUDE)
+
+# "facility_longitude
+unique(dataDFO$FACILITY_LONGITUDE)
+
+# startyear"
+unique(dataDFO$START_DATE)
+
+# "endyear" 
+unique(dataDFO$END_DATE)
+
+
+# Sheet 3 -----
+colnames(templSheet3)
+
+# "facilityID" ???
+
+# "CUID" 
+unique(dataDFO$STOCK_CU_ID)
+
+
+
 
 
 

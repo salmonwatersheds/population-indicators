3c3841649773e27c89b4ad7567aeb896f561017d
Bruno Carturan <bcarturan@psf.ca>
2023-11-29 12:00:17 -0800

    Wrote the code to create the comparison_bioStatus_Smsy_Smsy80 figures.

--- biological-status/code/biological_status_proba_analyses.R
+++ biological-status/code/biological_status_proba_analyses.R
@@ -79,10 +79,11 @@ biological_status_df <- rbind_biologicalStatusCSV_fun(pattern = pattern,
                                                       wd_output = wd_output,
                                                       region = region,
                                                       species_all = species_all)
+printFig <- F
 
 #
-write.csv(biological_status_df,paste0(wd_output,"/Biological_status_HBSRM_all.csv"),
-          row.names = F)
+# write.csv(biological_status_df,paste0(wd_output,"/Biological_status_HBSRM_all.csv"),
+#           row.names = F)
 
 head(biological_status_df)
 colnames(biological_status_df)
@@ -95,13 +96,16 @@ sum(gsub("_"," ",biological_status_df$CU) != biological_status_df$CU_dfo)/nrow(b
 sum(biological_status_df$CU_pse != biological_status_df$CU_dfo)/nrow(biological_status_df)
 
 # CUs with "Only NAs in cuspawnerabundance.csv for this CU" 
-biological_status_df[!is.na(biological_status_df$comment) & biological_status_df$comment == "Only NAs in cuspawnerabundance.csv for this CU" ,]
+biological_status_df[!is.na(biological_status_df$comment) & 
+                       biological_status_df$comment == "Only NAs in cuspawnerabundance.csv for this CU",]
 
 # CUs with "Not recent enough data"
-biological_status_df[!is.na(biological_status_df$comment) & grepl("Not recent enough data",biological_status_df$comment),]
+biological_status_df[!is.na(biological_status_df$comment) &
+                       grepl("Not recent enough data",biological_status_df$comment),]
 
 # rest of the CUs with data:
-biological_status_df <- biological_status_df[is.na(biological_status_df$comment) | biological_status_df$comment == "",] # ?! there should not be NAs...
+biological_status_df <- biological_status_df[is.na(biological_status_df$comment) |
+                                               biological_status_df$comment == "",] # ?! there should not be NAs...
 nrow(biological_status_df) # 113
 
 colToRemove_biostatus <- c("CU_pse","CU_dfo","genLength_available","comment")
@@ -109,7 +113,8 @@ colToRemove_biostatus <- c("CU_pse","CU_dfo","genLength_available","comment")
 biological_status_df <- biological_status_df[,!colnames(biological_status_df) %in% colToRemove_biostatus]
 
 # CUs that have contrasting biological status between Smsy80 and Smsy:
-colnamesSelect <- c("region","species","CU",colnames(biological_status_df)[grepl("Smsy_",colnames(biological_status_df))])
+colnamesSelect <- c("region","species","CU",
+                    colnames(biological_status_df)[grepl("Smsy_",colnames(biological_status_df))])
 
 biological_status_df$status_Smsy <- sapply(X = 1:nrow(biological_status_df), 
                                           FUN = function(r){
@@ -130,6 +135,86 @@ biological_status_df$status_Smsy80 <- sapply(X = 1:nrow(biological_status_df),
 # 
 biological_status_df[biological_status_df$status_Smsy != biological_status_df$status_Smsy80,]
 # that's not a lot of CUs!
+
+# Figure
+rDiff <- nrow(biological_status_df[biological_status_df$status_Smsy != biological_status_df$status_Smsy80,])
+rSame <- nrow(biological_status_df[biological_status_df$status_Smsy == biological_status_df$status_Smsy80,])
+
+table_region <- NULL
+
+regions <- unique(biological_status_df$region)
+for(rg in regions){
+  # rg <- regions[1]
+  biological_status_df_cut <- biological_status_df[biological_status_df$region == rg,]
+  
+  tableHere <- data.frame(region = rg,
+                          n_same = sum(biological_status_df_cut$status_Smsy == biological_status_df_cut$status_Smsy80),
+                          n_diff = sum(biological_status_df_cut$status_Smsy != biological_status_df_cut$status_Smsy80))
+  if(is.null(table_region)){
+    table_region <- tableHere
+  }else{
+    table_region <- rbind(table_region,tableHere)
+  }
+}
+
+table_region_m <- as.matrix(table_region[,c(2:3)])
+rownames(table_region_m) <- table_region$region
+colours <- rainbow(n = nrow(table_region_m))
+if(printFig){
+  jpeg(paste0(wd_figures,"/comparison_bioStatus_Smsy_Smsy80_regions.jpg"), 
+       width = 20,height = 15, units = "cm", res = 300)
+}
+par(mar=c(5,4.5,3,0.5))
+barplot(height = table_region_m,
+        main = "Comparison by regions",
+        ylab = "Number of CUs", xlab = "Biological status difference",
+        col = colours, 
+        names.arg = c("Same","Different"))
+legend("topright",rev(rownames(table_region_m)),fill = rev(colours), bty = "n")
+if(printFig){
+  dev.off()
+}
+
+# figure for species
+table_species <- NULL
+species <- unique(biological_status_df$species)
+for(sp in species){
+  # sp <- species[1]
+  biological_status_df_cut <- biological_status_df[biological_status_df$species == sp,]
+  
+  tableHere <- data.frame(species = sp,
+                          n_same = sum(biological_status_df_cut$status_Smsy == biological_status_df_cut$status_Smsy80),
+                          n_diff = sum(biological_status_df_cut$status_Smsy != biological_status_df_cut$status_Smsy80))
+  if(is.null(table_species)){
+    table_species <- tableHere
+  }else{
+    table_species <- rbind(table_species,tableHere)
+  }
+}
+
+table_species_m <- as.matrix(table_species[,c(2:3)])
+
+rownamesSp <- sapply(X = table_species$species, FUN = function(sp){
+  return(species_acronym_df$species_name[species_acronym_df$species_acro == sp][1])
+})
+rownames(table_species_m) <- rownamesSp
+colours <- rainbow(n = nrow(table_species_m))
+if(printFig){
+  jpeg(paste0(wd_figures,"/comparison_bioStatus_Smsy_Smsy80_species.jpg"), 
+       width = 20,height = 15, units = "cm", res = 300)
+}
+par(mar=c(5,4.5,3,0.5))
+barplot(height = table_species_m,
+        main = "Comparison by species",
+        ylab = "Number of CUs", xlab = "Biological status difference",
+        col = colours, 
+        names.arg = c("Same","Different"))
+sp_legend <- paste(rev(rownames(table_species_m)),"                 ")
+legend("topright",sp_legend,fill = rev(colours), bty = "n")
+if(printFig){
+  dev.off()
+}
+
 
 #' Import the benchmark values associated with the HBSRM ------
 pattern <- "benchmarks_summary"

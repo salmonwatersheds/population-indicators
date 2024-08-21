136984fb4d66ed233584a1d1757c65e61380bf85
Bruno Carturan <bcarturan@psf.ca>
2024-08-21 09:45:56 -0700

    Added spawner stream data from Yukon, TBR, Columbia and SH to the NuSEDS data

--- spawner-surveys/code/4_datasets_for_PSE.R
+++ spawner-surveys/code/4_datasets_for_PSE.R
@@ -94,8 +94,25 @@ Reynolds_issues <- import_mostRecent_file_fun(wd = wd_output,
                                             pattern = "SFU_Escapement_issues")
 
 
+#'* Import the conservationunits_decoder.csv *
+datasetsNames_database <- datasetsNames_database_fun()
+
+#' Import the cuspawnerabundance.csv from population-indicators/data_input or 
+#' download it from the PSF database.
+#' To calculating current spawner abundance for biostatus assessment
+fromDatabase <- update_file_csv <- F
+
+#' Import the conservationunits_decoder.csv from population-indicators/data_input or 
+#' download it from the PSF database.
+#' # To obtain the generation length and calculate the the "current spawner abundance".
+conservationunits_decoder <- datasets_database_fun(nameDataSet = datasetsNames_database$name_CSV[1],
+                                                   fromDatabase = fromDatabase,
+                                                   update_file_csv = update_file_csv,
+                                                   wd = wd_pop_indic_data_input_dropbox)
+
+
 #
-# Combine nuseds_cuid_streamid with data_extra_Reynolds_lab -----
+# Combine nuseds_cuid_streamid with data_extra_Reynolds_lab (WAIT UNTIL AFTER PSE 2.0 RELEASE) -----
 #' - All populations were attributed a cuid (in data_extra_Reynolds_lab.R), 
 #' - several locations were new and do not have a GFE_ID.
 
@@ -315,7 +332,7 @@ nuseds_final <- import_mostRecent_file_fun(wd = wd_output,
 cond_cuid_na <- is.na(nuseds_final$cuid)
 sum(cond_cuid_na) # 1998
 cond_pointid_na <- is.na(nuseds_final$pointid)
-sum(cond_pointid_na) # 2726
+sum(cond_pointid_na) # 9388 2726
 cond_gfe_id_na <- is.na(nuseds_final$GFE_ID)    # 0
 sum(cond_gfe_id_na) # 0
 cond_streamid_na <- is.na(nuseds_final$streamid)
@@ -323,8 +340,6 @@ sum(cond_streamid_na) # 1998
 # cond_noNa <- !cond_cuid_na & !cond_pointid_na & !cond_streamid_na
 cond_noNa <- cond_cuid_na
 dataset_1part2 <- nuseds_final[!cond_noNa,]
-
-#' Remove Steelhead --> already removed in nuseds_data.collation.R
 
 # rename sys_nm to stream_name_pse
 colnames(dataset_1part2)[colnames(dataset_1part2) == "sys_nm_final"] <- "stream_name_pse"
@@ -340,7 +355,7 @@ for(i in 1:length(field_toChange)){
   colnames(dataset_1part2)[colnames(dataset_1part2) == field_toChange[i]] <- fields_new[i]
 }
 
-# remove NAs
+# remove NAs in stream_observed_count
 dataset_1part2 <- dataset_1part2[!is.na(dataset_1part2$stream_observed_count),]
 
 
@@ -368,22 +383,331 @@ dataset_1part2 <- dataset_1part2 %>%
           stream_name_pse,
           year)
 
+
+dataset_1part2_total <- dataset_1part2
+
+#'* Remove Steelhead * (already removed)
+#'# --> already removed in nuseds_data.collation.R
+cond <- dataset_1part2$species_name != "Steelhead"
+dataset_1part2_SH <-  dataset_1part2[!cond,]
+dataset_1part2 <- dataset_1part2[cond,]
+
+#'* Remove TBR, Columbia and Yukon *
+cond <- !dataset_1part2$region %in% c("Yukon","Columbia","Transboundary")
+dataset_1part2_TBR <-  dataset_1part2[dataset_1part2$region == c("Transboundary"),]
+dataset_1part2_YK <-  dataset_1part2[dataset_1part2$region == c("Yukon"),]
+dataset_1part2_CB <-  dataset_1part2[dataset_1part2$region == c("Columbia"),]
+dataset_1part2 <- dataset_1part2[cond,]
+
 head(dataset_1part2)
 sum(is.na(dataset_1part2$streamid)) # 0
 sum(is.na(dataset_1part2$cuid)) # 0
-sum(is.na(dataset_1part2$pointid)) # 5117
-
+sum(is.na(dataset_1part2$pointid)) # 5001  5117
 
 dim(dataset_1part2) 
-# 149828     17
+# 149827 149828     17
+
+head(dataset_1part2)
+
+#
+# Add datasets for SH, Columbia, TBR and Yukon -------
+#
+# https://salmonwatersheds.slack.com/archives/C03LB7KM6JK/p1723659667266449
+
+wd_TBR <- paste0(wd_X_Drive1_PROJECTS,"/1_Active/Transboundary/Data & Assessments/transboundary-data/output") # not -status
+TBR_data <- import_mostRecent_file_fun(wd = wd_TBR, pattern = "dataset_1part2")
+colnames(TBR_data)[colnames(TBR_data) == "survey_qual"] <- "survey_quality"
+#  2024-04-19
+head(TBR_data)
+
+wd_SH <- paste0(wd_X_Drive1_PROJECTS,"/1_Active/Steelhead/3_Data_Analysis/steelhead-status/output/pse-data")
+SH_data <- import_mostRecent_file_fun(wd = wd_SH, pattern = "dataset_1part2")
+#  2024-05-02
+SH_data <- import_mostRecent_file_fun(wd = wd_pop_indic_data_input_dropbox, 
+                                      pattern = "steelhead_dataset_1part2") # TEMPORARY
+head(SH_data)
+SH_data$region <- sapply(SH_data$CUID,FUN = function(cuid){
+  cond <- conservationunits_decoder$cuid == cuid
+  return(conservationunits_decoder$region[cond])
+})
+
+wd_columbia <- paste0(wd_X_Drive1_PROJECTS,"/1_Active/Columbia/data & analysis/analysis/columbia-status/Output")
+Columbia_data <- import_mostRecent_file_fun(wd = wd_columbia, pattern = "dataset_1part2")
+# 2024-02-12
+Columbia_data <- import_mostRecent_file_fun(wd = wd_pop_indic_data_input_dropbox, 
+                                            pattern = "columbia_dataset_1part2") # TEMPORARY
+head(Columbia_data)
+Columbia_data$region <- "Columbia"
+
+wd_yukon <- paste0(wd_X_Drive1_PROJECTS,"/1_Active/Yukon/Data & Assessments/yukon-status/output")
+Yukon_data <- import_mostRecent_file_fun(wd = wd_yukon, pattern = "dataset_1part2")
+# 2022-12-19 
+Yukon_data <- import_mostRecent_file_fun(wd = wd_pop_indic_data_input_dropbox, 
+                                         pattern = "yukon_dataset_1part2") # TEMPORARY
+head(Yukon_data)
+Yukon_data$region <- "Yukon"
+
+#' * Import stream level data quality (dataset386_output) *
+# datasetsNames_database <- datasetsNames_database_fun()
+# 
+# fromDatabase <- update_file_csv <- F
+# 
+# dataset386_output <- datasets_database_fun(nameDataSet = datasetsNames_database$name_CSV[22],
+#                                            fromDatabase = fromDatabase,
+#                                            update_file_csv = update_file_csv,
+#                                            wd = wd_pop_indic_data_input_dropbox)
+
+# TEMPORARY SOLUTION cause the code above does not work
+dataset386_output <- import_mostRecent_file_fun(wd = wd_pop_indic_data_input_dropbox,
+                                                pattern = "dataset386")
+
+head(dataset386_output)
+
+# Check that streamid is unique for a cuid & stream location
+# 1) no streamid can be associated to multiple cuid:
+data_check <- dataset386_output[,c("cuid","OLD_streamid")] |> unique()
+sum(duplicated(data_check$OLD_streamid)) # 0 OK
+# 2) no streamid can be associated to multiple SYSTEM_SITE
+data_check <- dataset386_output[!is.na(dataset386_output$SYSTEM_SITE),c("OLD_streamid","SYSTEM_SITE")] |> unique()
+sum(duplicated(data_check$OLD_streamid)) # 0 OK
+
+
+#' * Merge Yukon_data, SH_data and Columbia_data & clean them *
+data_merged <- rbind(Yukon_data,SH_data,Columbia_data)
+data_merged <- data_merged[,-1]
+colnames(data_merged)[colnames(data_merged) == "NuSEDS.counts.by.stream"] <- "stream_observed_count"
+colnames(data_merged)[colnames(data_merged) == "CUID"] <- "cuid"
+colnames(data_merged)[colnames(data_merged) == "Species"] <- "species_name"
+colnames(data_merged)[colnames(data_merged) == "streamname"] <- "stream_name_pse"
+data_merged$species_abbr <- sapply(data_merged$cuid, FUN = function(cuid){
+  cond <- conservationunits_decoder$cuid == cuid
+  return(conservationunits_decoder$species_abbr[cond])
+})
+data_merged$cu_name_pse <- sapply(data_merged$cuid, FUN = function(cuid){
+  cond <- conservationunits_decoder$cuid == cuid
+  return(conservationunits_decoder$cu_name_pse[cond])
+})
+
+
+#'* survey_method" & "survey_quality & survey_score *
+# Ask Eric
+data_merged$survey_method <- NA
+data_merged$survey_quality <- NA
+data_merged$survey_score <- NA
+data_merged$comment <- ""
+for(r in 1:nrow(unique(data_merged[,c("OLD_streamid","cuid","year")]))){
+  # r <- 1
+  # if(any(unique(data_merged[,c("OLD_streamid","cuid","year")])$OLD_streamid[r] == 9755 &
+  #        #data_merged$year[r] == 2009 &
+  #        unique(data_merged[,c("OLD_streamid","cuid","year")])$cuid[r] == 481)){
+  #   break
+  # }
+  cond_streamid <- dataset386_output$OLD_streamid == unique(data_merged[,c("OLD_streamid","cuid","year")])$OLD_streamid[r]
+  cond_cuid <- dataset386_output$cuid == unique(data_merged[,c("OLD_streamid","cuid","year")])$cuid[r]
+  cond_year <- dataset386_output$year == unique(data_merged[,c("OLD_streamid","cuid","year")])$year[r]
+  
+  if(unique(data_merged[,c("OLD_streamid","cuid","year")])$OLD_streamid[r] == 9755){
+    cond_cuid <- dataset386_output$cuid == 480 # old cuid for Nass SH Ksi Gingolx (https://salmonwatersheds.slack.com/archives/CJ5RVHVCG/p1724198353599609?thread_ts=1724089498.307439&cid=CJ5RVHVCG)
+  }
+  
+  cond_total <- cond_streamid & cond_cuid & cond_year
+  
+  if(any(cond_total)){
+    cond <- data_merged$OLD_streamid == unique(data_merged[,c("OLD_streamid","cuid","year")])$OLD_streamid[r] &
+      data_merged$cuid == unique(data_merged[,c("OLD_streamid","cuid","year")])$cuid[r] &
+      data_merged$year == unique(data_merged[,c("OLD_streamid","cuid","year")])$year[r]
+    
+    data_merged$survey_method[cond] <- dataset386_output$survey_method[cond_streamid & cond_cuid & cond_year]
+    data_merged$survey_quality[cond] <- dataset386_output$survey_qual[cond_streamid & cond_cuid & cond_year]
+    data_merged$survey_score[cond] <- dataset386_output$survey_score[cond_streamid & cond_cuid & cond_year]
+    
+    if(is.na(dataset386_output$survey_method[cond_streamid & cond_cuid & cond_year])){
+      data_merged$comment[cond] <- "NA value"
+    }
+    
+  }else{
+    comment <- ""
+    if(!any(cond_streamid)){
+      cond <- data_merged$OLD_streamid == unique(data_merged[,c("OLD_streamid","cuid","year")])$OLD_streamid[r]
+      comment <- "streamid missing"
+ 
+    }else if(!any(cond_streamid & cond_year)){
+      cond <- data_merged$OLD_streamid == unique(data_merged[,c("OLD_streamid","cuid","year")])$OLD_streamid[r] & 
+        data_merged$year == unique(data_merged[,c("OLD_streamid","cuid","year")])$year[r]
+      comment <- "year missing, streamid present"
+      
+    }else if(!any(cond_cuid)){
+      cond <- data_merged$OLD_streamid == unique(data_merged[,c("OLD_streamid","cuid","year")])$OLD_streamid[r] & 
+        data_merged$year == unique(data_merged[,c("OLD_streamid","cuid","year")])$year[r]
+      comment <- "cuid missing, year present, streamid present"
+      
+    }
+    
+    data_merged$comment[cond] <- comment
+  }
+}
+
+# View(data_merged)
+sum(is.na(data_merged$survey_score))  # 930
+sum(!is.na(data_merged$survey_score)) # 410
+
+# Check if there is any "" comment without values
+cond <- data_merged$comment == "" & is.na(data_merged$survey_score)
+data_merged[cond,] # no --> all good
+
+# return the region, species, cuid and years 
+comments <- data_merged$comment |> unique()
+comments <- comments[comments != ""]
+for(c in comments){
+  cond_c <- data_merged$comment == c
+  colHere <- c("region","species_name","cu_name_pse","cuid","OLD_streamid","stream_name_pse")
+  if(c %in% c("year missing, streamid present","cuid missing, year present, streamid present")){ # "NA value"
+    colHere <- c(colHere,"year")
+  }
+  out <- data_merged[cond_c,colHere] |> unique()
+  print(c)
+  print(out)
+  print("")
+}
+
+# TODO: we just leave NAs for now until Eric eventually get back to me
+# https://salmonwatersheds.slack.com/archives/CJ5RVHVCG/p1724198353599609?thread_ts=1724089498.307439&cid=CJ5RVHVCG
+
+
+#'* GFE_ID (The PSE does not care about GFE_ID so far) *
+#'# https://salmonwatersheds.slack.com/archives/CJ5RVHVCG/p1724108249417019
+# Try to find GFE_ID 
+data_merged$GFE_ID <- NA
+for(r in 1:nrow(unique(data_merged[,c("region","stream_name_pse")]))){
+  region <- unique(data_merged[,c("region","stream_name_pse")])$region[r]
+  stream_name_pse <- unique(data_merged[,c("region","stream_name_pse")])$stream_name_pse[r]
+  cond_rg <- dataset_1part2_total$region == region
+  cond_str <- dataset_1part2_total$stream_name_pse == stream_name_pse
+  
+  if(!any(cond_rg & cond_str)){
+    cond_str <- simplify_string_fun(dataset_1part2_total$stream_name_pse) == simplify_string_fun(stream_name_pse)
+  }
+  
+  if(!any(cond_rg & cond_str)){
+    gfeid <- NA
+  }else{
+    gfeid <- dataset_1part2_total$GFE_ID[cond_rg & cond_str] |> unique()
+    if(length(gfeid) > 1){
+      print("STOP: More than one GFE_ID")
+      break
+    }
+    data_merged$GFE_ID[r] <- gfeid
+  }
+}
+
+sum(is.na(data_merged$GFE_ID)) # 1267
+sum(!is.na(data_merged$GFE_ID)) # 73
+
+
+#'* pointid & streamid *
+data_merged$pointid <- NA
+data_merged$streamid <- data_merged$OLD_streamid
+
+
+#'* Merge data_merged with TBR_data *
+colnames(TBR_data)[!colnames(TBR_data) %in% colnames(data_merged)]
+colnames(data_merged)[!colnames(data_merged) %in% colnames(TBR_data)]
+
+# Add species_abbr
+TBR_data$species_abbr <- sapply(X = TBR_data$cuid, function(cuid){
+  cond <- conservationunits_decoder$cuid == cuid
+  return(conservationunits_decoder$species_abbr[cond])
+})
+
+# Add GFE_ID, 
+TBR_data$GFE_ID <- NA
+for(r in 1:nrow(unique(TBR_data[,c("region","stream_name_pse")]))){
+  region <- unique(TBR_data[,c("region","stream_name_pse")])$region[r]
+  stream_name_pse <- unique(TBR_data[,c("region","stream_name_pse")])$stream_name_pse[r]
+  cond_rg <- dataset_1part2_total$region == region
+  cond_str <- dataset_1part2_total$stream_name_pse == stream_name_pse
+  
+  if(!any(cond_rg & cond_str)){
+    cond_str <- simplify_string_fun(dataset_1part2_total$stream_name_pse) == simplify_string_fun(stream_name_pse)
+  }
+  
+  if(!any(cond_rg & cond_str)){
+    gfeid <- NA
+  }else{
+    gfeid <- dataset_1part2_total$GFE_ID[cond_rg & cond_str] |> unique()
+    if(length(gfeid) > 1){
+      print("STOP: More than one GFE_ID")
+      break
+    }
+    TBR_data$GFE_ID[r] <- gfeid
+  }
+}
+sum(is.na(TBR_data$GFE_ID)) # 1522
+sum(!is.na(TBR_data$GFE_ID)) # 36
+  
+# Add pointid
+TBR_data$pointid <- NA
+  
+# Add survey score
+data_merged[,c("survey_method","survey_quality","survey_score")] |> unique()
+TBR_data$survey_quality |> unique()
+
+TBR_data$survey_score <- sapply(TBR_data$survey_quality, function(sq){
+  if(sq == "Low"){
+    out <- 5
+  }else if(sq == "Medium-Low"){
+    out <- 4
+  }else if(sq == "Medium"){
+    out <- 3
+  }else if(sq == "Medium-High"){
+    out <- 2
+  }else if(sq == "High"){
+    out <- 1
+  }else if(sq == "Unknown"){
+    out <- "Unknown"
+  }
+  return(out)
+})
+
+# Merge data_merged and TBR_data
+data_merged <- data_merged[,colnames(TBR_data)]
+data_merged <- rbind(data_merged,TBR_data)
+nrow(data_merged) # 2898
+
+# check if there are streamid in common between data_merged and dataset_1part2
+# --> no , all good then
+cond <- data_merged$streamid %in% dataset_1part2$streamid
+data_merged$streamid[cond]
+
+
+# merge  data_merged and dataset_1part2
+colnames(dataset_1part2)[!colnames(dataset_1part2) %in% colnames(data_merged)]
+colnames(data_merged)[!colnames(data_merged) %in% colnames(dataset_1part2)]
+
+data_merged <- data_merged[,colnames(dataset_1part2)]
+
+dataset_1part2_final <- rbind(dataset_1part2,data_merged)
+
+# order fields:
+dataset_1part2_final <- dataset_1part2_final %>% 
+  arrange(factor(region, levels = c("Yukon","Transboundary","Haida Gwaii","Nass",
+                                    "Skeena","Central Coast",
+                                    "Vancouver Island & Mainland Inlets",
+                                    "Fraser","Columbia")),
+          species_name,
+          cu_name_pse,
+          factor(indicator, levels = c("Y","N","",NA)),
+          stream_name_pse,
+          year)
+
 
 #
 # Save dataset_1part2_DATE.csv -----
 
 date <- as.character(Sys.time())
 date <- strsplit(x = date, split = " ")[[1]][1]
-date <- gsub("-","",date)
-write.csv(dataset_1part2,paste0(wd_output,"/dataset_1part2_",date,".csv"),
+# date <- gsub("-","",date)
+write.csv(dataset_1part2_final,paste0(wd_output,"/dataset_1part2_",date,".csv"),
           row.names = F)
 
 dataset_1part2 <- import_mostRecent_file_fun(wd = wd_output,
